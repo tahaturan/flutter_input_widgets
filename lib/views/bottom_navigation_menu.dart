4e5a53cf26abf5bc5337d68998e58fc8e14b1234
Taha Turan <tahaaturann16@gmail.com>
2022-06-14 05:33:52 +0300

    Parcalama Islemi Yapildi

---
+++ lib/views/bottom_navigation_menu.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class BottomNavMenu extends StatefulWidget {
+  final Function secilenMenuItem;
+  const BottomNavMenu({required this.secilenMenuItem, Key? key})
+      : super(key: key);
+
+  @override
+  State<BottomNavMenu> createState() => _BottomNavMenuState();
+}
+
+class _BottomNavMenuState extends State<BottomNavMenu> {
+  int secilenMenuItem = 0;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: secilenMenuItem,
+      onTap: (int index) {
+        setState(() {
+          secilenMenuItem = index;
+          widget.secilenMenuItem(secilenMenuItem);
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+            backgroundColor: Colors.blue),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+            backgroundColor: Colors.red),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add), label: "Add", backgroundColor: Colors.green),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: "Profile",
+          backgroundColor: Colors.indigo,
+          activeIcon: Icon(Icons.edit),
+        )
+      ],
+      backgroundColor: Colors.blue,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.greenAccent,
+      selectedFontSize: 15,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.shifting,
+    );
+  }
+}
--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_input_widgets/screen/page_one.dart';
 import 'package:flutter_input_widgets/screen/page_three.dart';
 import 'package:flutter_input_widgets/screen/page_two.dart';
+import 'package:flutter_input_widgets/views/bottom_navigation_menu.dart';
 import 'package:flutter_input_widgets/widgets/home_page.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,19 +13,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int secilenMenuItem = 0;
   static const List<Widget> pageList = [
     HomePage(),
     PageTwo(),
     PageThree(),
     PageFour(),
   ];
-  int secilenMenuItem = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      secilenMenuItem = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,36 +28,9 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('AnaSayfa'),
       ),
       body: pageList.elementAt(secilenMenuItem),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: secilenMenuItem,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-              backgroundColor: Colors.blue),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: "Search",
-              backgroundColor: Colors.red),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: "Add",
-              backgroundColor: Colors.green),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-            backgroundColor: Colors.indigo,
-            activeIcon: Icon(Icons.edit),
-          )
-        ],
-        backgroundColor: Colors.blue,
-        unselectedItemColor: Colors.white,
-        selectedItemColor: Colors.greenAccent,
-        selectedFontSize: 15,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.shifting,
-      ),
+      bottomNavigationBar: BottomNavMenu(secilenMenuItem: (int gelenDeger) {
+        secilenMenuItem = gelenDeger;
+      }),
     );
   }
 }
